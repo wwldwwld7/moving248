7defb625597c20731f74a56c7fd3405f32306978
Hyunjoongkimisback <122500532+Hyunjoongkimisback@users.noreply.github.com>
2023-07-24 17:49:16 +0900

    ok
    Former-commit-id: a3bd60a7dbeb8113851e26f41f5fb6b102feddac

---
+++ ec2dockertest/ec2dockertest/src/test/java/com/example/ec2dockertest/Ec2dockertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ec2dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2dockertestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
