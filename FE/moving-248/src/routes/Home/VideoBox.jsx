c9613e9c935b8ef412978cb2f623661245912a56
SSH <ssh5212@naver.com>
2023-08-03 16:02:35 +0900

    feat: 메인 페이지 디자인 & 기능 구현 최종 완료 S09P12B301-174
    Former-commit-id: 02898eb755cc7847a7a877bb2b83116b16bf3494

---
+++ FE/moving-248/src/routes/Home/VideoBox.jsx
@@ -0,0 +1,27 @@
+import './VideoBox.css';
+
+export default function VideoBox({ imgSrc = 'move' }) {
+    return (
+        <div className='video-box'>
+            {/* <!-- [S] Movie --> */}
+            <div className='jb-box '>
+                <video muted autoPlay loop>
+                    <source src={require('../../assets/video/truck.mp4')} type='video/mp4' />
+                    <strong>Your browser does not support the video tag.</strong>
+                </video>
+
+                <div className='jb-text'>
+                    <p className='vtext-big'>Move on 248</p>
+                    <p className='vtext-small'>당신의 새로운 설레임에 함께 합니다.</p>
+                </div>
+
+                <div className='scroll-downs'>
+                    <div className='mousey'>
+                        <div className='scroller'></div>
+                    </div>
+                </div>
+            </div>
+            {/* <!-- [E] Movie --> */}
+        </div>
+    );
+}
