ed7d16f39dbb3004f9f5648af344c42df29c2ae9
김라현 <wwldwwld7@naver.com>
2023-08-11 17:34:56 +0900

    Update FE/moving-248/src/routes/ApplyDetail/ApplyDetail.jsx
    Former-commit-id: 251160a19456d9dfb94204bd23672a3a332d3c5d
    Former-commit-id: 9819f85cbd4bce0b05ce5e943a651d374a4eb952

--- FE/moving-248/src/routes/ApplyDetail/ApplyDetail.jsx
+++ FE/moving-248/src/routes/ApplyDetail/ApplyDetail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ImgBox from '../../components/ImgBox/ImgBox';
 import './ApplyDetail.css';
 import { useNavigate } from 'react-router-dom';
