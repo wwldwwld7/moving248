c15a88c901bef12472666203241ec6991d512858
AngelPlayer <ssh5212@naver.com>
2023-08-12 21:46:08 +0900

    style: 디자인 수정
    Former-commit-id: 3192a3e8e46af87630890d2b8bc4dd20dfb6b888
    Former-commit-id: 4de84b393f33b3d2feedc69428d481bd4559588f

--- FE/moving-248/src/routes/MyPage/PartnerMyPage.jsx
+++ FE/moving-248/src/routes/MyPage/PartnerMyPage.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from 'react';
-// import ReactDom from 'react-dom';
 import PartnerMyPageDetail from '../../components/MyPage/PartnerMyPageDetail';
-import Header from '../../components/header/Header';
 import ImgBox from '../../components/ImgBox/ImgBox';
 
 const PartnerMyPage = props => {
     return (
         <>
-            <Header />
             <ImgBox imgSrc='moving-box' imgTitle='파트너 마이페이지' />
             <section className='max-container section'>
                 <PartnerMyPageDetail />
